e8c8c360d8defd6e4d152e5e522028ef440e7100
Charles Girard <charles.girard@polymtl.ca>
2016-12-03 12:28:21 -0500

    #1 terminé

--- test.c
+++ test.c
@@ -5,48 +5,46 @@ unsigned int fonction(unsigned int i){
 	asm
 	(
 		//(crypted & 0xf0000000) >> 12
-		"movl %0, %ecx   \n"
-		"movl %ecx, %ebx          \n"
+		"movl 8(%ebp), %ebx          \n"
 		"andl $0xf0000000, %ebx   \n"
 		"shr  $12, %ebx           \n"
 		"movl %ebx, %eax          \n"
 		//(crypted & 0xf000000)  >> 16
-		"movl %ecx, %ebx          \n"
+		"movl 8(%ebp), %ebx          \n"
 		"andl $0x0f000000, %ebx   \n"
 		"shr  $16, %ebx           \n"
 		"orl %ebx, %eax           \n"
 		//(crypted & 0xf00000)   >> 16
-		"movl %ecx, %ebx          \n"
+		"movl 8(%ebp), %ebx          \n"
 		"andl $0x00f00000, %ebx   \n"
 		"shr  $16, %ebx           \n"
 		"orl %ebx, %eax           \n"
 		//(crypted & 0xf0000)    << 8
-		"movl %ecx, %ebx          \n"
+		"movl 8(%ebp), %ebx          \n"
 		"andl $0x000f0000, %ebx   \n"
 		"shl  $8, %ebx            \n"
 		"orl %ebx, %eax           \n"
 		//(crypted & 0xf000)     << 16
-		"movl %ecx, %ebx          \n"
+		"movl 8(%ebp), %ebx          \n"
 		"andl $0x0000f000, %ebx   \n"
 		"shl  $16, %ebx           \n"
 		"orl %ebx, %eax           \n"
 		//(crypted & 0xf00)      << 4
-		"movl %ecx, %ebx          \n"
+		"movl 8(%ebp), %ebx          \n"
 		"andl $0x00000f00, %ebx   \n"
 		"shl  $4, %ebx           \n"
 		"orl %ebx, %eax           \n"
 		//(crypted & 0xf0)       >> 4
-		"movl %ecx, %ebx          \n"
+		"movl 8(%ebp), %ebx          \n"
 		"andl $0x000000f0, %ebx   \n"
 		"shr  $4, %ebx            \n"
 		"orl %ebx, %eax           \n"
 		//(crypted & 0xf)        << 20
-		"movl %ecx, %ebx          \n"
-		"andl $0x0000000f, %ebx   \n"
-		"shl  $20, %ebx           \n"
-		"orl %ebx, %eax           \n"
-		: "=q" (j)
-		: "g" (i)
+		"movl 8(%ebp), %ebx          \n"
+		"andl $0x0000000f, %ebx      \n"
+		"shl  $20, %ebx              \n"
+		"orl %ebx, %eax              \n"
+		"orl 12(%ebp), %eax            "
 	);
 	//return decrypted;
 }
--- tp5/tp5.c
+++ tp5/tp5.c
@@ -27,51 +27,52 @@ unsigned int Decryption_fct(unsigned int crypted,unsigned int key)
 */
 	unsigned int operations;
  	unsigned int resultatTmp;
-	
+	// J'aurais pu mieux faire manuellement, mais c'est la manière la mieux
+	// optimisée que j'ai pu obtenir en assembleur en ligne étendu.
  	asm volatile
  	(
- 		//(crypted & 0xf0000000) >> 12
- 		"movl %1, %2            \n"
- 		"andl $0xF0000000, %2   \n"
- 		"shrl $12, %2           \n"
- 		"movl %2, %3            \n"
- 		//(crypted & 0xf000000) >> 16 et (crypted & 0x0f00000) >> 16 =
- 		//(crypted & 0xff00000) >> 16
- 		"movl %1, %2            \n"
- 		"andl $0x0FF00000, %2   \n"
- 		"shrl $16, %2           \n"
- 		"orl  %2, %3            \n"
- 		//(crypted & 0xf0000)    << 8
- 		"movl %1, %2            \n"
- 		"andl $0x000F0000, %2   \n"
- 		"shll $8, %2            \n"
- 		"orl  %2, %3            \n"
- 		//(crypted & 0xf000)     << 16
- 		"movl %1, %2            \n"
- 		"andl $0x0000F000, %2   \n"
- 		"shll $16, %2           \n"
- 		"orl  %2, %3            \n"
- 		//(crypted & 0xf00)      << 4
- 		"movl %1, %2            \n"
- 		"andl $0x00000F00, %2   \n"
- 		"shll $4, %2            \n"
- 		"orl  %2, %3            \n"
- 		//(crypted & 0xf0)       >> 4
- 		"movl %1, %2            \n"
- 		"andl $0x000000F0, %2   \n"
- 		"shrl $4, %2            \n"
- 		"orl  %2, %3            \n"
- 		//(crypted & 0xf)        << 20
- 		"movl %1, %2            \n"
- 		"andl $0x0000000F, %2   \n"
- 		"shll $20, %2           \n"
- 		"orl  %2, %3            \n"
- 		//resultatTmp |= key
- 		"orl  %4, %3            \n"
- 		//decrypted = resultatTmp
- 		"movl %3, %0              "
- 		: "=q"(decrypted)
- 		: "g" (crypted), "r" (operations), "r" (resultatTmp), "g" (key)
+		//(crypted & 0xf0000000) >> 12
+		"movl %1, %2            \n"
+		"andl $0xF0000000, %2   \n"
+		"shrl $12, %2           \n"
+		"movl %2, %3            \n"
+		//(crypted & 0xf000000) >> 16 et (crypted & 0x0f00000) >> 16 =
+		//(crypted & 0xff00000) >> 16
+		"movl %1, %2            \n"
+		"andl $0x0FF00000, %2   \n"
+		"shrl $16, %2           \n"
+		"orl  %2, %3            \n"
+		//(crypted & 0xf0000)    << 8
+		"movl %1, %2            \n"
+		"andl $0x000F0000, %2   \n"
+		"shll $8, %2            \n"
+		"orl  %2, %3            \n"
+		//(crypted & 0xf000)     << 16
+		"movl %1, %2            \n"
+		"andl $0x0000F000, %2   \n"
+		"shll $16, %2           \n"
+		"orl  %2, %3            \n"
+		//(crypted & 0xf00)      << 4
+		"movl %1, %2            \n"
+		"andl $0x00000F00, %2   \n"
+		"shll $4, %2            \n"
+		"orl  %2, %3            \n"
+		//(crypted & 0xf0)       >> 4
+		"movl %1, %2            \n"
+		"andl $0x000000F0, %2   \n"
+		"shrl $4, %2            \n"
+		"orl  %2, %3            \n"
+		//(crypted & 0xf)        << 20
+		"movl %1, %2            \n"
+		"andl $0x0000000F, %2   \n"
+		"shll $20, %2           \n"
+		"orl  %2, %3            \n"
+		//resultatTmp |= key
+		"orl  %4, %3            \n"
+		//decrypted = resultatTmp
+		"movl %3, %0              "
+		: "=q"(decrypted)
+		: "g" (crypted), "r" (operations), "r" (resultatTmp), "g" (key)
  	);
 	return decrypted;
 }
